5b031bb5f140b017f9acf74041554281b8c43fa2
Marcelo Wiebbelling <mrodrigow@gmail.com>
2023-05-02 18:46:36 +0200

    Add button

--- components/AddMatchButton.vue
+++ components/AddMatchButton.vue
@@ -1,3 +1,31 @@
 <template>
-    <v-btn>Marcar Pontos</v-btn>
+    <v-btn color="primary">Marcar Pontos</v-btn>
+    <v-dialog v-model="dialog" activator="parent" width="auto">
+        <v-card>
+            <v-card-text>
+                <v-text-field type="number" v-for="name in store.names" :label="name" :rules="[rules.scores]"></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+                <v-btn color="primary" block @click="dialog = false">Adicionar</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </template>
+<script>
+import { useCanastraStore } from '@/stores/canastra'
+export default ({
+    setup() {
+        return {
+            store: useCanastraStore(),
+        }
+    },
+    data() {
+        return {
+            dialog: false,
+            rules: {
+                scores: value => !!value || 'Obrigatório',
+            },
+        }
+    },
+})
+</script>
