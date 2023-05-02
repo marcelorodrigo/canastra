c3b74640354a0c96e1d65887a2e15331fa76a7e7
Marcelo Wiebbelling <mrodrigow@gmail.com>
2023-05-02 16:09:32 +0200

    Logo

--- app.vue
+++ app.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
-      <SeoKit />
-      <OgImageScreenshot />
-      <OgImageStatic />
-      Oie
+    <SeoKit />
+    <OgImageScreenshot />
+    <OgImageStatic />
+    <v-app>
+      <v-app-bar app>
+        <img src="logo.svg" width="48" height="48" alt="Pontos da Canastra" title="Pontos da Canastra" />
+        <v-app-bar-title shrink-on-scroll class="pl-2 text-no-wrap d-none d-sm-flex">Pontos da Canastra</v-app-bar-title>
+        <v-spacer />
+      </v-app-bar>
+      <v-main>
+        <v-container fluid>
+          Oie
+        </v-container>
+      </v-main>
+    </v-app>
   </div>
-</template>  
+</template>
