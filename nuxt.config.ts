2c556437780ed87903f535bc97add95ed6a35797
Marcelo Wiebbelling <mrodrigow@gmail.com>
2023-05-02 15:02:24 +0200

    Vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modules: [
+        '@invictus.codes/nuxt-vuetify'
+    ],
     extends: [
         'nuxt-seo-kit'
     ],
     runtimeConfig: {
         indexable: true,
         public: {
-          siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://marcelorodrigo.github.io/canastra',
-          siteName: 'Pontos da Canastra',
-          siteDescription: 'Um aplicativo para que você possa contar os pontos das suas partidas de Canastra',
-          language: 'pt',   
+            siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://marcelorodrigo.github.io/canastra',
+            siteName: 'Pontos da Canastra',
+            siteDescription: 'Um aplicativo para que você possa contar os pontos das suas partidas de Canastra',
+            language: 'pt',
         }
-      },
+    },
+    vuetify: {
+        /* vuetify options */
+        vuetifyOptions: {
+            // @TODO: list all vuetify options
+        },
+        moduleOptions: {
+            /* nuxt-vuetify module options */
+            treeshaking: true,
+            useIconCDN: true,
+            /* vite-plugin-vuetify options */
+            styles: true,
+            autoImport: true,
+        }
+    }
 })
