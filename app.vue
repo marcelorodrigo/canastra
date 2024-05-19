f22ad22714f4f5f2abfcb476ec5a921f96655783
Marcelo Wiebbelling <mrodrigow@gmail.com>
2024-05-19 21:44:16 +0200

    Icons

--- app.vue
+++ app.vue
@@ -1,34 +1,44 @@
 <template>
   <div>
-    <NuxtPwaManifest />
-    <NuxtLoadingIndicator />
+    <NuxtPwaManifest/>
+    <NuxtLoadingIndicator/>
     <v-app>
-      <v-app-bar app :elevation="2" color="#ffcc66">
-        <img src="/logo.svg" width="60" height="60" alt="Pontos da Canastra" title="Pontos da Canastra" />
+      <v-app-bar app :elevation="4" color="#ffcc66">
+        <img src="/logo.svg" width="60" height="60" alt="Pontos da Canastra" title="Pontos da Canastra"
+             @click="toggleTheme"/>
         <v-app-bar-title class="text-no-wrap d-sm-flex">Pontos da Canastra</v-app-bar-title>
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
       </v-app-bar>
       <v-main>
         <v-container fluid class="d-flex flex-column">
-          <Matches v-show="!showStartMatch" />
-          <AddTeams v-show="showStartMatch" />
-          <AddMatchButton v-show="!showStartMatch" />
+          <Matches v-show="!showStartMatch"/>
+          <AddTeams v-show="showStartMatch"/>
+          <AddMatchButton v-show="!showStartMatch"/>
         </v-container>
       </v-main>
     </v-app>
   </div>
 </template>
 <script lang="ts">
-import { useCanastraStore } from '@/stores/canastra'
+import '@mdi/font/css/materialdesignicons.css'
+import {useCanastraStore} from '@/stores/canastra'
+import {useTheme} from "vuetify";
+
 export default {
   setup() {
     return {
       store: useCanastraStore(),
+      theme: useTheme(),
     }
   },
   computed: {
     showStartMatch(): boolean {
       return this.store.teams === 0
+    }
+  },
+  methods: {
+    toggleTheme() {
+      this.theme.dark = !this.theme.dark
     }
   }
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { md1 } from 'vuetify/blueprints'
 
 export default defineNuxtConfig({
     app: {
@@ -9,13 +8,13 @@ export default defineNuxtConfig({
                 content: "Fp7R6n_J3xcccADnN8uc3fX3_ewGLF-FwUrYsdW85Ms"
             }],
             link: [
-                { rel: "preconnect", href: "https://cdn.jsdelivr.net" },
-                { rel: "icon", href: "favicon.ico", type: "image/x-icon" },
-                { rel: "icon", href: "android-chrome-192x192.png", type: "image/png", sizes: "192x192" },
-                { rel: "icon", href: "android-chrome-521x512.png", type: "image/png", sizes: "512x512" },
-                { rel: "icon", href: "apple-touch-icon.png", type: "image/png", sizes: "180x180" },
-                { rel: "icon", href: "favicon-16x16.png", type: "image/png", sizes: "16x16" },
-                { rel: "icon", href: "favicon-32x32.png", type: "image/png", sizes: "32x32" },
+                {rel: "preconnect", href: "https://cdn.jsdelivr.net"},
+                {rel: "icon", href: "favicon.ico", type: "image/x-icon"},
+                {rel: "icon", href: "android-chrome-192x192.png", type: "image/png", sizes: "192x192"},
+                {rel: "icon", href: "android-chrome-521x512.png", type: "image/png", sizes: "512x512"},
+                {rel: "icon", href: "apple-touch-icon.png", type: "image/png", sizes: "180x180"},
+                {rel: "icon", href: "favicon-16x16.png", type: "image/png", sizes: "16x16"},
+                {rel: "icon", href: "favicon-32x32.png", type: "image/png", sizes: "32x32"},
             ]
         }
     },
@@ -39,12 +38,14 @@ export default defineNuxtConfig({
     vuetify: {
         /* vuetify options */
         vuetifyOptions: {
-            blueprint: md1
+            icons: {
+                defaultSet: 'mdi',
+            }
         },
         moduleOptions: {
             /* nuxt-vuetify module options */
             treeshaking: true,
-            useIconCDN: true,
+            useIconCDN: false,
             /* vite-plugin-vuetify options */
             styles: true,
             autoImport: true,
