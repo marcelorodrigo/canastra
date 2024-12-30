6199be31e9c75fbac569aa56b65657b2611c10f5
Marcelo Wiebbelling <mrodrigow@gmail.com>
2024-12-30 20:18:48 +0100

    PWA Configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,35 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Pontos da Canastra',
+        short_name: 'Pontos Canastra',
+        description: 'Marcador de pontos para o jogo de Canastra',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
