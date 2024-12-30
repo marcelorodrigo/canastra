8266657bada220f55b23d4838190a54d7c876bb8
Marcelo Wiebbelling <mrodrigow@gmail.com>
2024-12-30 17:21:32 +0100

    Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
-    <header class="bg-orange-300 shadow-md p-4 flex items-center justify-center">
-        <img src="@/assets/logo.svg" alt="Canastra" class="h-10 w-10" />
-        <h1 class="ml-2 text-xl font-bold text-gray-800">Marcador de Canastra</h1>
-    </header>
+  <header class="bg-orange-300 shadow-md p-4 flex items-center justify-center">
+    <img src="@/assets/logo.svg" alt="Canastra" class="h-10 w-10" />
+    <h1 class="ml-2 text-xl font-bold text-gray-800">Marcador de Canastra</h1>
+  </header>
 </template>
