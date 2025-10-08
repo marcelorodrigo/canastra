4f0caf2730b25ee464ba8f681a20c57892e32adc
Marcelo Wiebbelling <mrodrigow@gmail.com>
2025-10-08 16:01:29 +0200

    Vite PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({ registerType: 'autoUpdate' }),
     tailwindcss(),
     vueDevTools(),
   ],
