a4b874b60d29120ad94f71a9ae054c6f429f4151
Marcelo Wiebbelling <mrodrigow@gmail.com>
2025-10-08 16:30:45 +0200

    Unit tests

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -1,11 +1,17 @@
 import { describe, it, expect } from 'vitest'
-
 import { mount } from '@vue/test-utils'
+import { createPinia } from 'pinia'
 import App from '../App.vue'
 
 describe('App', () => {
   it('mounts renders properly', () => {
-    const wrapper = mount(App)
-    expect(wrapper.text()).toContain('You did it!')
+    const wrapper = mount(App, {
+      global: {
+        plugins: [createPinia()]
+      }
+    })
+    const h1 = wrapper.find('h1')
+    expect(h1.exists()).toBe(true)
+    expect(h1.text()).toBe('Marcador de Canastra')
   })
 })
